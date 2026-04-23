70ccbbc4528d22d07d4e6e8353362a0f14012b38
CX33071 <3541053286@qq.com>
2026-04-23 22:29:59 +0800

    note

--- Timestamp.h
+++ Timestamp.h
@@ -3,5 +3,12 @@
 #include <ctime>
 #include <sys/time.h>
 namespace muduo{
-    
+    namespace base{
+        class Timestamp{
+            private:
+             int64_t microSecondsSinceEpoch_;
+            public:
+             static const int kMicroSecondsPerSecond = 1000000;
+        }
+    }
 }

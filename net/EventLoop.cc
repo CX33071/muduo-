9f98a5d86410995c1375df281637e2ffe9cd39f9
CX33071 <3541053286@qq.com>
2026-05-06 16:17:11 +0800

    note

---
+++ net/EventLoop.cc
@@ -0,0 +1,143 @@
+#include "EventLoop.h"
+#include <iostream>
+#include <sys/epoll.h>
+#include "Channel.h"
+#include "Epoller.h"
+#include <sys/eventfd.h>
+#include "../base/logger.h"
+#include <unistd.h>
+#include <assert.h>
+using namespace muduo::net;
+thread_local EventLoop* t_loopInThisThread = nullptr;//thread_local=每个线程独有一份，一个线程只能有一个EventLoop
+IgnoreSigPipe __on;//全局忽略SIGPIPE信号，防止写已关闭socket导致程序崩溃
+EventLoop::EventLoop()
+    : looping_(false),
+      quit_(false),
+      iteration_(0),
+      threadId_(std::this_thread::get_id()),//保存创建时的线程ID
+      poller_(std::make_unique<Epoller>(this)),//创建Epoller
+      wakeupFd_(createEventfd()),//创建eventfd
+      wakeupChannel_(new Channel(this,wakeupFd_))//包装成Channel
+      {
+        //一个线程智能有一个EventLoop
+        if(t_loopInThisThread){
+            LOG_FATAL << "线程里已有EventLoop";
+        }else{
+            t_loopInThisThread = this;
+        }
+        //设置wakeupfd的读回调
+        wakeupChannel_->setReadCallback([this](Timestamp) { handleRead(); });
+        wakeupChannel_->enableReading();//监听读事件
+}
+EventLoop::~EventLoop(){
+    wakeupChannel_->disableAll();//取消所有事件
+    t_loopInThisThread = nullptr;//线程标记清空
+}
+void EventLoop::loop(int timeout=-1){//整个muduo的主循环
+    assertInLoopThread();
+    looping_ = true;
+    while(!quit_){
+        activeChannels_.clear();
+        pollReturnTime_ = poller_->poll(timeout, activeChannels_);//epoll_wait
+        for(auto channel:activeChannels_){//处理所有活跃channel的事件
+            channel->handleEvent(pollReturnTime_);
+        }
+        dopPendingFunctors();//执行跨线程任务
+    }
+    looping_ = false;
+}
+void EventLoop::quit(){
+    quit_ = true;
+    if(!isInLoopThread()){
+        wakeup();//唤醒epoll,让它退出阻塞
+    }
+}
+boll EventLoop::isInLoopThread()const{
+    return std::this_thread::get_id() == threadId_;
+}
+void EventLoop::assertInLoopThread(){
+    if(!isInLoopThread()){
+        abortNotInLoopThread();
+    }
+}
+void EventLoop::abortNotInLoopThread(){
+    LOG_FATAL << "EventLoop::abortNotInLoopThread - EventLoop " << this
+              << " was created in threadId_ = " << threadId_
+              << ", current thread id = " << std::this_thread::get_id();
+}
+void EventLoop::runInLoop(const Functor &cb){
+    if(isInLoopThread()){//当前就是IO线程，直接执行
+        cb();
+    }else{
+        queueInLoop(cb);//丢到队列，异步执行
+    }
+}
+void EventLoop::wakeup(){//发送唤醒信号
+    uint64_t one = 1;
+    ssize_t n = write(wakeupFd_, &one, sizeof one);
+    if(n!=sizeof(one)){
+        LOG_ERROR << "EventLoop::wakeup() writes " << n
+                  << " bytes instead of 8";
+    }
+}
+void EventLoop::queueInLoop(const Functor &cb){
+    {
+        std::lock_guard<std::mutex> lock(mutex_);
+        pendingFunctors_ - emplace_back(cb);//加入待执行队列
+    }
+    // 如果不是在本线程，或者正在调用 functors（说明是嵌套），就唤醒 EventLoop
+    if (!isInLoopThread() || callingPendingFunctors_) {
+        wakeup();
+    }
+}
+int EventLoop::createEventfd(){//创建一个事件fd,专门用来唤醒epoll
+    int evtfd = eventfd(0, EFD_NONBLOCK | EFD_CLOEXEC);
+    if(evtfd<0){
+        LOG_SYSERR << "Failed in eventfd";
+        abort();
+    }
+    return evtfd;
+}
+void EventLoop::handleRead(){//读出写进eventfd的值，清空事件，避免epoll一直触发
+    uint64_t one = 1;
+    ssize_t n = read(wakeupFd_, &one, sizeof one);
+    if(n!sizeof(one)){
+        LOG_ERROR << "EventLoop::handleRead() reads " << n
+                  << " bytes instead of 8";
+    }
+}
+void EventLoop::doPendingFunctors(){
+    std::vector<Functor> functors;
+    callingPendingFunctors_ = true;
+    {
+        std::unique_lock lock(mutex_);
+        //交换队列，减少锁临界区
+        functors.swap(pendingFunctors_);
+    }
+    for(auto functor : functors){
+        functor();
+    }
+    callingPendingFunctors_ = false;
+}
+//转发给Epoller的函数,EventLoop不做实际工作，全部交给Epoller
+void EventLoop::updateChannel(Channel*channel){
+    poller_->updateChannel(channel);
+}
+void EventLoop::removeChannel(Channel*channel){
+    poller_->removeChannel(channel);
+}
+//定时任务，交给TimerQueue执行
+TimerId EventLoop::runAt(const Timestamp &time,const Timer::TimerCallback&cb){
+    return timerQueue_->addTimer(cb, time, 0);
+}
+TimerId EventLoop::runAfter(double delay,const Timer::TimerCallback &cb){
+    Timestamp time(Timestamp::addTime(Timestamp::now(), delay));
+    return runAt(time, cb);
+}//非静态成员函数或变量必须依附于某个具体对象
+TImerId EventLoop::runEvery(double interval,const Timer::TimerCallback&cb){
+    Timestamp time(Timestamp::addTime(Timestamp::now(), interval));
+    return timerQueue_->addTimer(cb, time, interval);
+}
+void EventLoop::cancel(TimerId id){
+    return timerQueue_->cancle(id);
+}
---
+++ net/EventLoop.h
@@ -0,0 +1,59 @@
+#pragma once
+#include <thread>
+#include <mutex>
+#include "../base/noncopyable.h"
+#include "TimeId.h"
+#include "TimerQueue.h"
+#include "sigpipe.h"
+#include <memory>
+#include <vector>
+#include <atomic>
+namespace muduo{
+    namespace net{
+    class Epoller;
+    class Channel;
+    class EventLoop:noncopyable{//一个线程只能有一个EventLoop，不能复制
+        public:
+         EventLoop();
+         ~EventLoop();
+         void loop(int timeout);//灵魂，事件循环的死循环
+         void quit();
+         void assertInLoopThread();
+         bool isInLoopThread() const;
+         void updateChannel(Channel* channel);
+         void removeChannel(Channel* channel);
+         //定时器接口
+         TimerId runAt(const Timestamp& time, const Timer::TimerCallback& cb);//在指定时间执行
+         TimerId runAfter(double delay, const Timer::TimerCallback& cb);//延迟多久执行
+         TimerId runEvery(double interval, const Timer::TimerCallback& cb);//每隔多久执行
+         void cancel(TimerId id);//取消定时器
+         //跨线程执行任务
+         using Functor = std::function<void()>;
+         void runInLoop(const Functor& cb);
+         void queueInLoop(const Functor& cb);
+         //用eventfd唤醒阻塞在epoll_wait的EventLoop
+         void wakeup();
+         int createEventfd();
+        private:
+         void abortNotInLoopThread();
+         void handleRead();//执行wakeupfd读事件
+         void dopendingFunctors();//执行所有跨线程丢过来的任务
+         using ChannelList = std::vector<Channel*>;//活跃Channel列表
+         bool looping_;//是否处于loop()状态，是否已经在事件循环中
+         std::atomic<bool> quit_;//原子布尔，线程安全的退出标记
+         int64_t iteration_;//循环次数，调试或者统计用
+         Timestamp pollReturnTime_;//每轮poll返回时间戳，用于定时器判断
+         const std::thread::id threadId_;//创建该EventLoop的线程id,用于检查当前函数是不是在正确线程执行
+         std::unique_ptr<Epoller> poller_;//epoll封装器
+         ChannelList activeChannel_;//epoll返回的活跃Channel
+         //跨线程任务队列+锁
+         std::vector<Functor> pendingFunctors_;//延迟执行的任务队列
+         bool callingPendingFunctors_;//是否正在执行pendingFunctors_,防止嵌套调用
+         std::mutex mutex_;//保护pendingFunctors_的互斥锁
+         int wakeupFd_;
+         std::unique_ptr<Channel> wakeupChannel_;
+         std::unique_ptr<TimerQueue> timerQueue_;//定时器队列
+         //unique_ptr智能指针，自动delete,专属的对象，别人不能复制，不能共享
+    };
+    }  
+}

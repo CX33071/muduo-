0fae9712bf55561f93a5dc06bca29e4a4e5e833c
CX33071 <3541053286@qq.com>
2026-05-07 15:00:57 +0800

    note

---
+++ net/EventLoopThreadpool.cc
@@ -0,0 +1,22 @@
+#include "EventLoopThreadpool.h"
+using namespace muduo::net;
+EventLoopThreadPool::EventLoopThreadPool(EventLoop*baseloop):baseLoop_(baseloop),started_(false),numThreads_(0),next_(0){}
+EventLoopThreadPool::~EventLoopThreadPool(){}
+void EventLoopThreadPool::start(){
+    baseLoop_->assertInLoopThread();
+    started_ = true;
+    for (int i = 0; i < numThreads_;i++){
+        std::shared_ptr<EventLoopThread> thread(new EventLoopThread);
+        threads_.push_back(thread);
+        loops_.push_back(thread->startLoop());//启动线程，拿到里面的EventLoop。自动启动loop()循环，把loop指针存进loops_数组
+    }
+}
+EventLoop*EventLoopThreadPool::getNextLoop(){
+    baseLoop_->assertInLoopThread();
+    EventLoop* loop;
+    if(!loops_.empty()){
+        loop = loops_[next];//轮询取出下一个loop
+        next_ = (next_ + 1) % loops_.size();
+    }
+    return loop;
+}
